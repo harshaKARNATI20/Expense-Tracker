b960c9e20f3e223fe4196156c1bdfb33dcb95984
Harsha Karnati <114800403+harshaKARNATI20@users.noreply.github.com>
2025-07-05 00:53:55 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,8 @@
-// backend/config/db.js
-
 const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error("❌ MongoDB connection failed:", err.message);
